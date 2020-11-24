6d31de648ee8e2709d3b7646c0a83002d5c05d77
dsrules007 <sousilvaney@gmail.com>
2020-11-24 13:44:30 +0000

    deploy update mongoDB_uri

--- app.js
+++ app.js
@@ -176,6 +176,12 @@ app.post("/login", function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
+let port = process.env.PORT;
+
+if(port == null || port == ""){
+    port= 3000;
+}
+
+app.listen(port , function () {
     console.log("Server started");
 })
