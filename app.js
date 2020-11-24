47c246fc86f8fd9105400fa72047ec575ac234bd
dsrules007 <sousilvaney@gmail.com>
2020-11-24 15:19:46 +0000

    change google path to correct redirect

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://s-e-c-r-e-t-s.herokuapp.com/auth/google/secrets",
     userProfile: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
